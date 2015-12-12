78c78cfd161a6dc29b8b1966cfa85641c3682e88
jliu69 <liu_johnson@yahoo.com>
2015-12-12 14:44:06 -0500

    new files for the project

---
+++ P2S3/P2S3/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  P2S3
+//
+//  Created by Johnson Liu on 12/12/15.
+//  Copyright © 2015 HomeOffice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleBarButton: UIBarButtonItem!
+    
+    var logoImageView:UIImageView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBarHidden = true
+        
+        //-- logo
+        self.logoImageView = UIImageView.init(frame: CGRectMake(0, 0, 109, 44))
+        self.logoImageView!.backgroundColor = UIColor.clearColor()
+        self.logoImageView!.contentMode = UIViewContentMode.ScaleAspectFit
+        self.logoImageView!.image = UIImage(named: "p2s_action_bar_logo_small.png")
+        self.titleBarButton!.customView = self.logoImageView!
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBarHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func settingsAction(sender: AnyObject) {
+        print("... this is to settings page")
+    }
+    
+    
+}
+
